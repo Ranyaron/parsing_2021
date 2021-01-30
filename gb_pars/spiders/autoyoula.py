97f78e4274c2772636cd6ccc1423406b23e9d651
ran <ranyaron@mail.ru>
2021-01-30 12:57:58 +0300

    les4

--- gb_pars/spiders/autoyoula.py
+++ gb_pars/spiders/autoyoula.py
@@ -1,11 +1,22 @@
+import os
+import re
+import pymongo
 import scrapy
 from scrapy.http import Response
-
+from dotenv import load_dotenv
+from urllib.parse import urljoin
 
 class AutoyoulaSpider(scrapy.Spider):
     name = 'autoyoula'
     allowed_domains = ['auto.youla.ru']
     start_urls = ['https://auto.youla.ru/']
+
+    load_dotenv('.env')
+    data_base_url = os.getenv('DATA_BASE_URL')
+    data_client = pymongo.MongoClient(data_base_url)
+    data_base = data_client['parsing_2021']
+    collection = data_base[name]
+
     css_query = {
         "brand": "div.ColumnItemList_container__5gTrc a.blackLink",
         "pagination": "div.Paginator_block__2XAPy a.Paginator_button__u1e7D",
@@ -15,7 +26,14 @@ class AutoyoulaSpider(scrapy.Spider):
     data_query = {
         "title": "div.AdvertCard_advertTitle__1S1Ak::text",
         "price": "div.AdvertCard_price__3dDCr::text",
+        "photos": "img.PhotoGallery_photoImage__2mHGn::attr(src)",  # Список фото объявления (ссылки)
+        "specifications": [
+            "div.AdvertSpecs_row__ljPcX div.AdvertSpecs_label__2JHnS::text",
+            "div.AdvertSpecs_row__ljPcX div.AdvertSpecs_data__xK2Qx::text, div.AdvertSpecs_row__ljPcX div.AdvertSpecs_data__xK2Qx a::text"
+        ],  # Список характеристик
         "description": "div.AdvertCard_descriptionInner__KnuRi::text",
+        "author": "script::text",  # Ссылка на автора объявления
+        # "telephone": ""  # Телефон
     }
 
     @staticmethod
@@ -33,5 +51,18 @@ class AutoyoulaSpider(scrapy.Spider):
     def ads_parse(self, response: Response):
         data = {}
         for name, query in self.data_query.items():
-            data[name] = response.css(query).get()
-        print(1)
+            if name == "photos":
+                data[name] = response.css(query).getall()
+            elif name == "specifications":
+                query_lst_0 = response.css(query[0]).getall()
+                query_lst_1 = response.css(query[1]).getall()
+                data[name] = dict(zip(query_lst_0, query_lst_1))
+            elif name == "author":
+                script = response.css(query).getall()
+                result_re = re.findall(r"youlaId%22%2C%22([0-9a-zA-Z]+)%22%2C%22avatar", script[8])
+                url_user = "https://youla.ru/user/"
+                data[name] = urljoin(url_user, result_re[0])
+            else:
+                data[name] = response.css(query).get()
+
+        self.collection.insert_one(data)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scrapy.settings import Settings
 from gb_pars.spiders.autoyoula import AutoyoulaSpider
 
 if __name__ == "__main__":
+
     crawler_settings = Settings()
     crawler_settings.setmodule("gb_pars.settings")
     crawler_process = CrawlerProcess(settings=crawler_settings)
